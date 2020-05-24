962b4d70958e8bb7a0a591281511d8e726be60ab
w00tc <w00tcolors2@gmail.com>
2020-05-24 13:19:34 +0300

    Updates

---
+++ precache-manifest.5f86743f0f9b7148b26e8898888398d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3801cd822e4c595a57e102b65853209",
+    "url": "//w00tc.github.io/authReactRedux/index.html"
+  },
+  {
+    "revision": "90718a83b8b2f61650b9",
+    "url": "//w00tc.github.io/authReactRedux/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2a9ff393270384d44677",
+    "url": "//w00tc.github.io/authReactRedux/static/js/2.5689c8e2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "//w00tc.github.io/authReactRedux/static/js/2.5689c8e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90718a83b8b2f61650b9",
+    "url": "//w00tc.github.io/authReactRedux/static/js/main.8c7da37b.chunk.js"
+  },
+  {
+    "revision": "de9bebafb3c70578e615",
+    "url": "//w00tc.github.io/authReactRedux/static/js/runtime-main.33aeed1b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "//w00tc.github.io/my-app/precache-manifest.9aad5fdc1eb69979917d1754f43d04e3.js"
+  "//w00tc.github.io/authReactRedux/precache-manifest.5f86743f0f9b7148b26e8898888398d0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//w00tc.github.io/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//w00tc.github.io/authReactRedux/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
